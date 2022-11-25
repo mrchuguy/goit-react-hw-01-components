8c9cc3c76086cd04808b3251476edc1e71fad9a7
Sergey Chuguy <mr.chuguy@gmail.com>
2022-11-25 22:38:02 +0200

    add proptypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -10,6 +10,7 @@ import {
   Tag,
   UserName,
 } from './Profile.styled';
+import PropTypes from 'prop-types';
 
 export const Profile = ({
   username,
@@ -45,4 +46,16 @@ export const Profile = ({
       </StatsList>
     </ProfileCard>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
